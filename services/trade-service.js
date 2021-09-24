a4909b17193afc849153032d725dbb98e62d5f24
Amir Zametica - External <amir.zametica@klika.ba>
2021-09-24 20:17:13 +0200

    Remove unused method

--- services/trade-service.js
+++ services/trade-service.js
@@ -53,48 +53,6 @@ exports.getTradesByUserIdAndStatuses = async (userId, statuses = []) =>
     },
   ]);
 
-exports.getFinalizedTrades = async (userId) => {
-  return await Trade.aggregate([
-    {
-      $match: {
-        userId: mongoose.Types.ObjectId(userId),
-        status: { $in: ['closed', 'rewarded', 'sold'] },
-      },
-    },
-    { $sort: { createdAt: -1 } },
-    {
-      $lookup: {
-        localField: 'betId',
-        from: 'bets',
-        foreignField: '_id',
-        as: 'bet',
-      },
-    },
-    {
-      $project: {
-        betId: 1,
-        outcomeIndex: 1,
-        investmentAmount: 1,
-        outcomeTokens: 1,
-        status: 1,
-        createdAt: 1,
-        bet: {
-          $let: {
-            vars: {
-              betMatch: {
-                $arrayElemAt: ['$bet', 0],
-              },
-            },
-            in: {
-              finalOutcome: '$$betMatch.finalOutcome',
-            },
-          },
-        },
-      },
-    },
-  ]);
-};
-
 exports.closeTrades = async (userId, bet, outcomeIndex, status, session) => {
   if (![eventService.BET_STATUS.active].includes(bet.status)) {
     throw new Error(`Cannot close inactive bets, bet status is ${bet.status}`);
